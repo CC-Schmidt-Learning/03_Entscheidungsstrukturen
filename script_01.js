f319d20a10ed6b6ce7160e2583783cde3e6f1e90
Daniela <Daniela-Haeussler@gmx.de>
2022-11-10 09:22:45 +0100

    if Abfrage

--- script_01.js
+++ script_01.js
@@ -2,21 +2,43 @@
 
 // Entscheidungsstrukturen | control flow
 
-// Deklaration + Assigment
-const ageJohn = 35;
+// Deklaration + Assignment
+const ageJohn = 25;
 const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnYounger, isJohnEqual, isJohnUnequal;
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
+isJohnYounger = (ageJohn < ageMark);
+isJohnUnequal = (ageJohn != ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+//console.log("ageJohn: " + ageJohn);
+//console.log("ageMark: " + ageMark);
+//console.log("isJohnOlder: " + isJohnOlder);
+//console.log("isJohnEqual: " + isJohnEqual);
+//console.log("isJohnUnequal: " + isJohnUnequal);
+//console.log("------------------");
 
+// TINA = There is no alternative; 
+// Entweder JA oder NEIN; keine Alternative
+
+
+//if(isJohnOlder)
+//{
+//    console.log("John ist älter ");
+//}
+
+// IF - Else Abfrage //
+
+if(isJohnOlder)
+{
+   console.log("John ist älter ");
+}
+else (isJohnYounger)
+{
+    console.log("John ist jünger "); 
+};
